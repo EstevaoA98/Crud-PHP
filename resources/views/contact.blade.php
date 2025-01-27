e71789089dd951ac19fe00151b1676a1c1262909
Estevão Miranda <estevao.a98@gmail.com>
2025-01-27 15:43:45 -0300

    layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,2 +1,10 @@
+@extends('layouts.main')
+
+@section('title','EventoDev - Contato')
+
+@section('content')
+
 <h1>Pagina de contato</h1>
 <a href="/" li>voltar</a>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,64 @@
+<!-- filepath: /Users/estevaoandrade/Documents/GitHub/Crud-PHP/resources/views/layouts/main.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/sass/style.scss', 'resources/js/app.js'])
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <title>@yield('title')</title>
+    <!-- Boostrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+            <a class="navbar-brand col-lg-3 me-0" href="/">Eventos Dev</a>
+            <ul class="navbar-nav col-lg-6 justify-content-lg-center">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Eventos</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Inscreva</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">PHP</a></li>
+                  <li><a class="dropdown-item" href="#">JAVA</a></li>
+                  <li><a class="dropdown-item" href="#">SQL</a></li>
+                </ul>
+              </li>
+            </ul>
+            <div class="d-lg-flex col-lg-3 justify-content-lg-end">
+              <button class="btn btn-primary">Login</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+    @yield('content')
+</body>
+
+<footer class="text-center mt-auto py-4">
+    <div class="container">
+        <p class="mb-0"> EvntosDev {{ date('Y') }} &copy;
+        </p>
+    </div>
+</footer>
+
+</html>
