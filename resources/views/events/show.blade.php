cf0ae64958464dbc76cdaca2bc8799e6873c3c38
Estevão Miranda <estevao.a98@gmail.com>
2025-01-31 11:13:01 -0300

    css show board

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -6,19 +6,20 @@
 
     <div class="col-md-10 offset-md-1"></div>
     <div class="row">
-        <div id="imqge-container" class="col-md-6">
-            <img src="/img/events/{{$event->image}}" class="img-fluid" alt="{{$event->title}}">
+        <div id="imge-container" class="col-md-6">
+            <img src="/img/events/{{$event->image}}" class="img-events img-fluid" alt="{{$event->title}}">
         </div>
         <div id="info-container" class="col-md-6">
-            <h1>{{$event->title}}</h1>
-            <p class="event-location"><ion-icon name="location-outline"></ion-icon>{{$event->location}}</p>
-            <p class="events-participants"><ion-icon name="people-outline"></ion-icon>participantes</p>
+            <h1 class="event-title">{{$event->title}}</h1>
+            <br>
+            <p class="event-location"><ion-icon name="location-outline"></ion-icon> {{$event->location}}</p>
+            <p class="events-participants"><ion-icon name="people-outline"></ion-icon> participantes</p>
             <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Criador do evento</p>
-            <a href="#" class="btn btn-primary" id="event-submit">Confirmar presença</a>
-            <p></p>
+            <br>
+            <a href="#" class="btn btn-custom" id="event-submit">Confirmar presença</a>
         </div>
         <div class="col-md-12" id="description-container">
-            <h3>Sobre o evento</h3>
+            <h3 class="about">Sobre o evento</h3>
             <p class="event-description">{{$event->description}}/</p>
         </div>
     </div>
