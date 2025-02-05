4b15edf772394a949d8b428d79966e91d76127cd
Estevão Miranda <estevao.a98@gmail.com>
2025-02-05 00:50:05 -0300

    Add erro nos campos de criação

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,36 +5,31 @@
 @section('content')
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1 class="text-center">Adicionando Evento</h1>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         <form id="event-form" action="/events" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="form-group m-3">
                 <label for="title">Título do Evento:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento...">
+                <p class="alert alert-danger"> <ion-icon name="alert-circle"></ion-icon> {{ $errors->first('title') }}</p>
             </div>
             <div class="form-group m-3">
                 <label for="location">Cidade:</label>
                 <textarea class="form-control" id="location" name="location" placeholder="Local do evento..."></textarea>
+                <p class="alert alert-danger"> <ion-icon name="alert-circle"></ion-icon> {{ $errors->first('location') }}</p>
             </div>
             <div class="form-group m-3">
                 <label for="date">Data:</label>
                 <input type="date" class="form-control" id="date" name="date">
+                <p class="alert alert-danger"> <ion-icon name="alert-circle"></ion-icon> {{ $errors->first('date') }}</p>
             </div>
             <div class="form-group m-3">
                 <label for="description">Descrição:</label>
                 <textarea class="form-control" id="description" name="description" placeholder="Descrição do evento..."></textarea>
+                <p class="alert alert-danger"> <ion-icon name="alert-circle"></ion-icon> {{ $errors->first('description') }}</p>
             </div>
             <div class="form-group m-3">
                 <label for="description">Infraestrutura do Evento:</label><br>
-                
+                <p class="alert alert-danger"> <ion-icon name="alert-circle"></ion-icon> {{ $errors->first('items') }}</p>
                 <div class="form-grup">
                     <input type="checkbox" name="items[]" value="Salas para workshops"> Salas para workshops
                     <p class="infra">Ambientes específicos para hands-on e treinamentos.</p>
@@ -70,6 +65,7 @@
             <div class="form-group m-3">
                 <label for="image">Banner do evento:</label>
                 <input type="file" class="form-control" id="image" name="image">
+                <p class="alert alert-danger"> <ion-icon name="alert-circle"></ion-icon> {{ $errors->first('image') }}</p>
             </div>
             <div class="form-group text-center m-3">
                 <button type="submit" class="btn btn-custom" value="Criar evento">Criar Evento</button>
